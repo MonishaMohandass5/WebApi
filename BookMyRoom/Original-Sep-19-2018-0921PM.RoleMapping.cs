b242188308e09c8258fdadc4aeff551998da687b
Monisha Mohandass <m1047236@mindtree.com>
2018-10-01 09:55:51 +0530

    webapi

---
+++ BookMyRoom/Original-Sep-19-2018-0921PM.RoleMapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("HotelCityMapping")]
+    public class RoleMapping
+    {
+            [Key]
+            public int mapId { get; set; }
+            [ForeignKey("hotelDetails")]
+            public int hotelId { get; set; }
+            public HotelDetails hotelDetails { get; set; }
+            [ForeignKey("cityDetails")]
+            public int cityId { get; set; }
+            public CityDetails cityDetails { get; set; }
+        }
+    }
+}
---
+++ BookMyRoom/Recovered-Sep-19-2018-0921PM.RoleDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("RoleDetails")]
+     public class RoleDetails
+    {
+            [Key]
+            public int roleId { get; set; }
+            public string roleName { get; set; }
+    }
+}
